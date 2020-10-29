08c3f03142ac10ebe940361d0a6d8816b16d6878
Catherine Evans <catherineevans@catherieslaptop.dhcp.nd.edu>
2020-10-29 15:31:38 -0400

    Worked on FileSystem.h

--- include/FileSystem.h
+++ include/FileSystem.h
@@ -1,6 +1,8 @@
 #ifndef FILESYSTEM_H
 #define FILESYSTEM_H
 
+// author: Catherine Evans 
+
 #include "Edge.h"
 #include "File.h"
 #include "Vertex.h"
@@ -9,23 +11,59 @@
 #define CIN std::cin
 #define ENDL std::endl
 
-int main() {
-    unsigned int location = 0; //dont know what im doing here 
-    listOfCommands(); 
+// CREATE FILESYSTEM OBJECT
+// can't create without tree code yet 
+
+template<class T>
+class FileSystem {
+    private:
+        Graph<T> graph;
+        Tree<T> tree; 
+        int location; //!!!!!!!!!!!!!!!
+
+    public: 
+        void newTaxon(T data) {
+            tree.add_node(data); //no such method exists yet, change fxn name 
+        }
+
+        void newFile(File inputFile) {
+            graph.add_vert(inputFile)
+        }
+
+        void findFile(File inputFile) {
+            tempLocation = -1;
+            // is this the way you guys want to search 
+            for (int i = 0; i < graph.verts.length(); ++i) {
+                if (graph.verts[i] == inputFile) {
+                    tempLocation = graph.verts[i];
+                    location = tempLocation;
+                }
+            }
+            if (tempLocation == -1) {
+                COUT << "Our apologies, the requested file does not exist in your catalog. " << ENDL;
+            }
+            //should have some kind of location modifier so when you go to this node you stay, if user wants to add a file here or whatever 
+        } 
+
+        void findFileName(File inputFile) {
+            tempLoc = -1;
+            for (int j = 0; j < graph.verts.length(); ++j) {
+                if (graph.verts[j] == inputFile) {
+                    tempLoc = 1
+                    COUT << "The file exists under the category " //add the node on the tree here that it's at here
+                }
+            }
+            if (tempLoc == -1) {
+                COUT << "We're sorry! The requested file does not exist in your taxonomy." << ENDL;
+            }
+        } 
+
+        void findFileCategory() {
+            //can't do this yet 
+        }
 }
 
-void newTaxon() {
-
-}
-
-void newFile() {}
-
-void findFile() {} 
-
-void findFileName() {} 
-
-void findFileCategory() {}
-
+//not sure if this is the proper place to put this lol 
 void listOfCommands() {
     std::string input = "";
 
